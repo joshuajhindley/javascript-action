b8e7a4e337544976e236f8a01875a0a0f30fe841
Joshua Hindley <joshua.hindley2222@gmail.com>
2025-01-18 08:17:20 +1300

    Update project

--- src/main.js
+++ src/main.js
@@ -1,27 +1,57 @@
-import * as core from '@actions/core'
-import { wait } from './wait.js'
+const puppeteer = require("puppeteer")
+const { GoogleSpreadsheet } = require("google-spreadsheet")
+const { JWT } = require("google-auth-library");
 
-/**
- * The main function for the action.
- *
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run() {
-  try {
-    const ms = core.getInput('milliseconds')
+const updateSpreadsheet = async (world, us) => {
+  const serviceAccountAuth = new JWT({
+    email: process.env.CLIENT_EMAIL,
+    key: process.env.PRIVATE_KEY,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+  const doc = new GoogleSpreadsheet(process.env.SHEET_ID, serviceAccountAuth)
+  await doc.loadInfo(); // loads document properties and worksheets
+  const sheet = doc.sheetsByTitle[process.env.SHEET_NAME]
+  await sheet.loadCells('B1:B5')
+  const worldCell = sheet.getCellByA1('B1')
+  const usCell = sheet.getCellByA1('B2')
+  const time = sheet.getCellByA1('B4')
+  const date = sheet.getCellByA1('B5')
+  const now = new Date()
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  worldCell.value = world
+  usCell.value = us
+  time.value = now.toLocaleTimeString("en-NZ", { timezone: 'Pacific/Wellington' })
+  date.value = now.toLocaleDateString("en-NZ", { timezone: 'Pacific/Wellington' })
+  await sheet.saveUpdatedCells()
+
+  process.exit(0)
 }
+
+const scrapeData = async () => {
+  const browser = await puppeteer.launch({headless: 'new'})//({headless: false})
+  const page = await browser.newPage()
+
+  await page.goto(process.env.URL_1)
+
+  await page.waitForSelector('.sal-mip-quote__block-grid')
+  const world = await page.evaluate(() => {
+    const elem = document.getElementsByClassName('sal-dp-value')[0]
+    return Number(elem.textContent.split('/')[0].trim())
+  })
+  console.log(world)
+
+  
+  await page.goto(process.env.URL_2)
+  await page.waitForSelector('.sal-mip-quote__block-grid')
+  const us = await page.evaluate(() => {
+    const elem = document.getElementsByClassName('sal-dp-value')[0]
+    return Number(elem.textContent.split('/')[0].trim())
+  })
+  console.log(us)
+
+  await updateSpreadsheet(world, us)
+}
+
+scrapeData()
